d1ef9ef624335b0239fb5276eb5735164643e721
Amna Owais Ansari <160513158+Amna-Owais-Ansari@users.noreply.github.com>
2024-03-03 00:01:10 +0500

    Add files via upload

---
+++ Assignmnet Q#2/PersMessage.js
@@ -0,0 +1,5 @@
+/*Personal Message: Store a person’s name in a variable, and print a message to that person.
+Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”*/
+var name1 = "Eric";
+var message = ("\"Hello ".concat(name1, ",would you like to learn some Python today?\u201D"));
+console.log(message);
