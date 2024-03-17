34b9b6412a5faa24052f57e789f841abf29d6581
Amna Owais Ansari <160513158+Amna-Owais-Ansari@users.noreply.github.com>
2024-03-17 23:12:23 +0500

    Add files via upload

---
+++ Assignment Q#9/Favourite_Num.js
@@ -0,0 +1,7 @@
+/*Favorite Number:
+Store your favorite number in a variable. Then, using that variable,
+ create a message that reveals your favorite number.
+Print that message.*/
+var num = 102;
+var m = "My Favourite Number is ".concat(num);
+console.log(m);
---
+++ Assignment Q#9/Favourite_Num.ts
@@ -0,0 +1,8 @@
+/*Favorite Number: 
+Store your favorite number in a variable. Then, using that variable,
+ create a message that reveals your favorite number. 
+Print that message.*/
+
+let num: number = 102;
+let m: string = `My Favourite Number is ${num}`;
+console.log(m);
